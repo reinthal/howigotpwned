542e9cd93f48faa82617ef84c1e3f66bdd23b648
Alexander Reinthal <email@reinthal.me>
2024-09-06 23:50:58 +0200

    feat: add uploader

--- dagster_project/utils/unpack.py
+++ dagster_project/utils/unpack.py
@@ -1,55 +1,40 @@
-from minio import Minio
-from minio.error import S3Error
-import magic
-from io import BytesIO
-FOLDER_PATH='Cit0/Cit0day.in_special_for_xss.is/Cit0day Prem [_special_for_xss.is]/'
+import os
+from dagster import EnvVar
+import patoolib
+import tempfile
+from dagster_aws.s3 import S3Resource
 
+class Uploader:
 
-def unrar(object: BytesIO) -> List[]:
-
-# Function to process all objects in the specified bucket
-def process_objects(bucket, minio_client, mime_detector):
-    # List objects in the bucket
-    try:
-        objects = minio_client.list_objects(bucket, prefix=FOLDER_PATH)
-        for obj in objects:
-            # Get the object
-            if not obj.object_name.endswith('/'):
-                response = minio_client.get_object(bucket, obj.object_name)
-                # Use BytesIO to handle the object in memory
-                data = BytesIO(response.data)
-                
-                # Use 'python-magic' to determine the MIME type
-                mime_type = mime_detector.from_buffer(data.getvalue())
-                
-                # Print details
-                print(f'###\nObject Name: {obj.object_name}, \nMIME Type: {mime_type}')
-                
-                # Close the stream
-                data.close()
-                response.close()
-    except S3Error as err:
-        print("S3 Error:", err)
+    def __init__(self, s3: S3Resource, bucket: str):
+        self.s3 = s3
+        self.bucket = bucket
+    
+    def copy_archive_to_s3(self, archive_path, parent_key=""):
+        # Create filesystems
+        client = self.s3.get_client()
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            patoolib.extract_archive(archive_path, outdir=tmpdirname)
+            for f in os.listdir(tmpdirname):
+                print(f"{tmpdirname}/{f}")
+                Key = f if not parent_key else f"{parent_key}/{f}"
+                client.upload_file(f"{tmpdirname}/{f}", Bucket=self.bucket, Key=Key)
 
 def main():
-    import os 
-    # MinIO credentials and configuration
-    minio_url = os.getenv("S3_ENDPOINT")
-    aws_access_key_id = os.getenv("S3_ACCESS_KEY")
-    aws_secret_access_key = os.getenv("S3_SECRET_KEY")
-    bucket_name = 'leaks'
+    bucket_name = 'dev'
 
     # Connect to MinIO
-    minio_client = Minio(
-        "nas.reinthal.me:9000",
-        access_key=aws_access_key_id,
-        secret_key=aws_secret_access_key,
-        secure=False
+    nas_minio = S3Resource(
+        aws_secret_access_key=EnvVar("S3_SECRET_KEY").get_value(),
+        aws_access_key_id=EnvVar("S3_ACCESS_KEY").get_value(),
+        endpoint_url=EnvVar("S3_ENDPOINT").get_value(),
     )
+    ul = Uploader(nas_minio,bucket_name)
+    archive_path = "/home/kog/Downloads/zipcaars.com {1.170} [HASH] (Business)_special_for_XSS.IS.rar"
+    ul.copy_archive_to_s3(archive_path=archive_path)
 
-    # Initialize magic for MIME type detection
-    mime_detector = magic.Magic(mime=True)
-    process_objects(bucket_name,minio_client,mime_detector)
+    
+    
 
 if __name__ == '__main__':
     main()
