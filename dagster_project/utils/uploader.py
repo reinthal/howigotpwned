f2956532155843c8af5e569f333e6b753b6508c4
Alexander Reinthal <email@reinthal.me>
2024-09-15 20:52:09 +0200

    refactor to method

--- dagster_project/utils/uploader.py
+++ dagster_project/utils/uploader.py
@@ -4,20 +4,15 @@ import patoolib
 import tempfile
 from dagster_aws.s3 import S3Resource
 
-class Uploader:
 
-    def __init__(self, s3: S3Resource, destination_bucket: str):
-        self.s3 = s3
-        self.destination_bucket = destination_bucket
-    
-    def copy_archive_to_s3(self, archive_path, parent_key=""):
-        # Create filesystems
-        client = self.s3.get_client()
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            patoolib.extract_archive(archive_path, outdir=tmpdirname)
-            for f in os.listdir(tmpdirname):
-                Key = f if not parent_key else f"{parent_key}/{f}"
-                client.upload_file(f"{tmpdirname}/{f}", Bucket=self.destination_bucket, Key=Key)
+def copy_archive_to_s3(s3: S3Resource, destination_bucket: str, archive_path, parent_key=""):
+    # Create filesystems
+    client = s3.get_client()
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        patoolib.extract_archive(archive_path, outdir=tmpdirname)
+        for f in os.listdir(tmpdirname):
+            Key = f if not parent_key else f"{parent_key}/{f}"
+            client.upload_file(f"{tmpdirname}/{f}", Bucket=destination_bucket, Key=Key)
 
 def main():
     bucket_name = 'dev'
@@ -28,9 +23,8 @@ def main():
         aws_access_key_id=EnvVar("S3_ACCESS_KEY").get_value(),
         endpoint_url=EnvVar("S3_ENDPOINT").get_value(),
     )
-    ul = Uploader(nas_minio,bucket_name)
     archive_path = "/home/kog/Downloads/zipcaars.com {1.170} [HASH] (Business)_special_for_XSS.IS.rar"
-    ul.copy_archive_to_s3(archive_path=archive_path)
+    copy_archive_to_s3(nas_minio, bucket_name, archive_path=archive_path)
 
     
     
