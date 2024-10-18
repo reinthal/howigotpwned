c1947113c354ad01db4677d51cfcb99da019060f
Alexander Reinthal <email@reinthal.me>
2024-10-19 00:17:02 +0200

    feat: add a poc iceberg write

--- dagster_project/assets.py
+++ dagster_project/assets.py
@@ -9,43 +9,26 @@ from dagster_project.partitions import password_archive_partitions_def
 from dagster_project.utils import copy_archive_to_s3, get_objects
 from pyiceberg.catalog import load_catalog
 import polars as pl
-LEAKS_BUCKET='leaks'
 RAW_BUCKET='raw'
-FOLDER_PATH='Cit0/Cit0day.in_special_for_xss.is/Cit0day Prem [_special_for_xss.is]'
+FOLDER_PATH='extracted'
 
 @asset(group_name="raw")
 def cit0day_prem_special_for_xssis_archives(
-    context: AssetExecutionContext, 
-    
+    context: AssetExecutionContext,
+
     s3: S3Resource) -> List[str]:
     """Asset for all password dump archives"""
-    archives = get_objects(source_bucket=LEAKS_BUCKET, prefix=FOLDER_PATH, s3=s3)
+    archives = get_objects(source_bucket=RAW_BUCKET, prefix=FOLDER_PATH, s3=s3)
 
     context.instance.add_dynamic_partitions(password_archive_partitions_def.name, \
             partition_keys=archives)
     return archives
 
-@asset(
-    partitions_def=password_archive_partitions_def,
-    group_name="raw",
-    deps=[cit0day_prem_special_for_xssis_archives]
-)
-def cit0day_uncompressed(context: AssetExecutionContext, s3: S3Resource):
-    archive = context.partition_key
-    
-    with tempfile.NamedTemporaryFile() as tf:
-        s3.get_client().download_file(LEAKS_BUCKET, archive, tf.name) 
-        copy_archive_to_s3(s3, RAW_BUCKET, tf.name, parent_key=archive)
-    
-    context.log.info(f"Uploaded {archive} to `{RAW_BUCKET}`")
-    return s3.get_client().list_objects
-
-
 # get all objects in raw
 @asset(
     partitions_def=password_archive_partitions_def,
     group_name="staging",
-    deps=[cit0day_uncompressed]
+    deps=[cit0day_prem_special_for_xssis_archives]
 )
 def cit0day_password_files(
     context: AssetExecutionContext, s3: S3Resource
@@ -66,14 +49,14 @@ def cit0day_password_files(
         "prefix": pl.String()
         })
     catalog = load_catalog("default",
-           **{
-             "warehouse": EnvVar("DESTINATION__CATALOG__WAREHOUSE").get_value(),
-              "uri": EnvVar("DESTINATION__CATALOG__URI").get_value(),
-              "py-io-impl": "pyiceberg.io.pyarrow.PyArrowFileIO",
-              "s3.endpoint": EnvVar("DESTINATION__CATALOG__S3_ENDPOINT").get_value(),
-              "s3.access-key-id": EnvVar("DESTINATION__CATALOG__ACCESS_KEY_ID").get_value(),
-              "s3.secret-access-key": EnvVar("DESTINATION__CATALOG__SECRET_ACCESS_KEY").get_value(),
-              "type": EnvVar("DESTINATION__CATALOG__TYPE").get_value()
+        **{
+            "warehouse": "s3://iceberg/warehouse",
+            "uri": "https://nessie.local.reinthal.cc/iceberg",
+            "py-io-impl": "pyiceberg.io.pyarrow.PyArrowFileIO",
+            "s3.endpoint": EnvVar("DESTINATION__FILESYSTEM__CREDENTIALS__AWS_S3_ENDPOINT").get_value(),
+            "s3.access-key-id": EnvVar("DESTINATION__FILESYSTEM__CREDENTIALS__AWS_ACCESS_KEY_ID").get_value(),
+            "s3.secret-access-key": EnvVar("DESTINATION__CATALOG__SECRET_ACCESS_KEY").get_value(),
+            "type": "rest"
     })
     catalog.create_namespace_if_not_exists("staging")
     table = catalog.create_table_if_not_exists("staging.cit0day_password_files", schema=password_files_pyarrow_schema)
@@ -89,7 +72,7 @@ def cit0day_password_files(
         file_obj = BytesIO()
         s3.get_client().download_fileobj(RAW_BUCKET, file_name, file_obj)
         df = pl.read_csv(file_obj, has_header=False, separator=":", schema=password_files_polars_schema)
-        
+
         pa_df = df.with_columns(
             (pl.lit(RAW_BUCKET)).alias("bucket"),
             (pl.lit(file_name)).alias("prefix")
@@ -97,4 +80,3 @@ def cit0day_password_files(
         table.append(pa_df)
 
     return df
-    
