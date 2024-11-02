d47df3851f1d6a161ec26ae3c4e56917260ac27a
Alexander Reinthal <email@reinthal.me>
2024-11-02 21:28:22 +0100

    fix: remove kuberntes configuration

--- dagster_project/__init__.py
+++ dagster_project/__init__.py
@@ -3,27 +3,12 @@ from dagster import (
     Definitions,
     define_asset_job,
 )
-from dagster_k8s import k8s_job_executor
 
 from dagster_project.assets import (
     cit0day_password_files,
     cit0day_prem_special_for_xssis_archives,
 )
 from dagster_project.resources import nas_minio, nessie_default_catalog
-from dagster_project.utils.environment import is_local_environment
-
-job_executor = k8s_job_executor.configured(
-    {
-        "step_k8s_config": {
-            "container_config": {
-                "resources": {
-                    "requests": {"cpu": "2", "memory": "2Gi"},
-                }
-            }
-        },
-        "max_concurrent": 5,  # Set your desired limit here
-    }
-)
 
 # Define a job that targets asset_a and all its upstream dependencies
 cit0day_job = define_asset_job(
@@ -42,5 +27,5 @@ defs = Definitions(
         "nessie_default": nessie_default_catalog,
     },
     assets=[cit0day_prem_special_for_xssis_archives, cit0day_password_files],
-    executor=None if is_local_environment() else job_executor,
+    executor=None,
 )  # noqa: E501
