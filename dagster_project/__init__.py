2b292691ad78e4de0a98f7947fadb65742829b68
Alexander Reinthal <email@reinthal.me>
2024-11-09 15:46:29 +0100

    feat(dagster): add ttl

--- dagster_project/__init__.py
+++ dagster_project/__init__.py
@@ -1,45 +1,14 @@
-from dagster import (
-    AssetSelection,
-    Definitions,
-    define_asset_job,
-)
-
+from dagster import Definitions
+from dagster_project.k8s_config import job_executor
 from dagster_project.utils.environment import is_local_environment
+
+from dagster_project.jobs import cit0day_job
 from dagster_project.assets import (
     cit0day_password_files,
     cit0day_prem_special_for_xssis_archives,
 )
-from dagster_k8s import k8s_job_executor
 from dagster_project.resources import nas_minio, nessie_default_catalog
 
-
-job_executor = k8s_job_executor.configured(
-    {
-        "step_k8s_config": {
-            "container_config": {
-                "resources": {
-                    "requests": {"cpu": "2", "memory": "2Gi"},
-                }
-            }
-        },
-        "max_concurrent": 20,
-    }
-)
-
-# Define a job that targets asset_a and all its upstream dependencies
-cit0day_job = define_asset_job(
-    name="cit0day_job",
-    selection=AssetSelection.assets(
-        cit0day_prem_special_for_xssis_archives
-    ).downstream(),
-)
-cit0day_job = cit0day_job.with_tags(
-    {
-        "dagster-k8s/config": {
-            "job_spec_config": {"ttl_seconds_after_finished": 60}  # Set TTL to 1 minute
-        }
-    }
-)
 
 defs = Definitions(
     jobs=[cit0day_job],
--- dagster_project/assets.py
+++ dagster_project/assets.py
@@ -6,6 +6,7 @@ import polars as pl
 from dagster import AssetExecutionContext, asset
 from dagster_aws.s3 import S3Resource
 
+from dagster_project.k8s_config import job_ttl
 from dagster_project.partitions import password_archive_partitions_def
 from dagster_project.resources import NessieCatalogResource
 from dagster_project.schemas import (
