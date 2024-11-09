eb0d2f71be113128066c426f2eb3e47fd2823f64
Alexander Reinthal <email@reinthal.me>
2024-11-09 18:05:56 +0100

    chore: linting

--- dagster_project/jobs.py
+++ dagster_project/jobs.py
@@ -3,13 +3,11 @@ from dagster import (
     define_asset_job,
 )
 
-from dagster_project.assets import  cit0day_password_files
+from dagster_project.assets import cit0day_password_files
 from dagster_project.k8s_config import job_ttl
 
 cit0day_job = define_asset_job(
     name="cit0day_job",
     tags=job_ttl,
-    selection=AssetSelection.assets(
-       cit0day_password_files
-    )
+    selection=AssetSelection.assets(cit0day_password_files),
 )
